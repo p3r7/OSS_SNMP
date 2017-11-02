231562167ed149e3575b580156da12ef054cb3c6
jordan.besly <jordan.besly@transatel.com>
2017-11-02 14:47:41 +0100

    Dedicated classes for linkset and siglink

---
+++ src/OSS_SNMP/MIBS/Squire/STP/LinkSet.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace OSS_SNMP\MIBS\Squire\STP;
+
+/**
+ * A class for performing SNMP V2 queries on Squire Signal Transfer Point, LinkSet resource
+ */
+class LinkSet extends \OSS_SNMP\MIB
+{
+    const OID_BASE                              = '.1.3.6.1.2.1.2.2.10';
+
+	const OID_STATUS                            = '.1.3.6.1.2.1.2.2.10.0';
+	const OID_FLAGS                             = '.1.3.6.1.2.1.2.2.10.1';
+	const OID_TYPE                              = '.1.3.6.1.2.1.2.2.10.2';
+	const OID_MAP                               = '.1.3.6.1.2.1.2.2.10.3';
+	const OID_ORIGINATING                       = '.1.3.6.1.2.1.2.2.10.4';
+	const OID_POINTCODE                         = '.1.3.6.1.2.1.2.2.10.5';
+	const OID_NI                                = '.1.3.6.1.2.1.2.2.10.6';
+	const OID_COMBINED                          = '.1.3.6.1.2.1.2.2.10.7';
+	const OID_IN_SERVICE                        = '.1.3.6.1.2.1.2.2.10.8';
+	const OID_OUT_SERVICE                       = '.1.3.6.1.2.1.2.2.10.9';
+	const OID_TFX_REQUIRED                      = '.1.3.6.1.2.1.2.2.10.10';
+	const OID_TRANSFER_REPORT                   = '.1.3.6.1.2.1.2.2.10.11';
+	const OID_ADD_TRANSFER_REPORT               = '.1.3.6.1.2.1.2.2.10.12';
+	const OID_ADD_MTP_ROUTES                    = '.1.3.6.1.2.1.2.2.10.13';
+	const OID_MTP_CDR                           = '.1.3.6.1.2.1.2.2.10.14';
+	const OID_TX_MSU_TOTAL                      = '.1.3.6.1.2.1.2.2.10.15';
+	const OID_TX_MSU_SEC                        = '.1.3.6.1.2.1.2.2.10.16';
+	const OID_TX_MSU_SEC_MAX                    = '.1.3.6.1.2.1.2.2.10.17';
+	const OID_RX_MSU_TOTAL                      = '.1.3.6.1.2.1.2.2.10.18';
+	const OID_RX_MSU_SEC                        = '.1.3.6.1.2.1.2.2.10.19';
+	const OID_RX_MSU_SEC_MAX                    = '.1.3.6.1.2.1.2.2.10.20';
+	const OID_TX_BITS_TOTAL                     = '.1.3.6.1.2.1.2.2.10.21';
+	const OID_TX_BITS_SEC                       = '.1.3.6.1.2.1.2.2.10.22';
+	const OID_TX_BITS_SEC_MAX                   = '.1.3.6.1.2.1.2.2.10.23';
+	const OID_RX_BITS_TOTAL                     = '.1.3.6.1.2.1.2.2.10.24';
+	const OID_RX_BITS_SEC                       = '.1.3.6.1.2.1.2.2.10.25';
+	const OID_RX_BITS_SEC_MAX                   = '.1.3.6.1.2.1.2.2.10.26';
+	const OID_DESCRIPTION                       = '.1.3.6.1.2.1.2.2.10.27';
+	const OID_OPTIONS                           = '.1.3.6.1.2.1.2.2.10.28';
+	const OID_COO_BIS                           = '.1.3.6.1.2.1.2.2.10.29';
+	const OID_M3UA_AS                           = '.1.3.6.1.2.1.2.2.10.30';
+	const OID_IC_L3LABEL                        = '.1.3.6.1.2.1.2.2.10.31';
+	const OID_OG_L3LABEL                        = '.1.3.6.1.2.1.2.2.10.32';
+	const OID_TX_BITS_THROTTLE                  = '.1.3.6.1.2.1.2.2.10.33';
+	const OID_TX_MSU_THROTTLE                   = '.1.3.6.1.2.1.2.2.10.34';
+	const OID_TX_CONGESTION                     = '.1.3.6.1.2.1.2.2.10.35';
+	const OID_TX_BINNED                         = '.1.3.6.1.2.1.2.2.10.36';
+
+
+    /**
+     * Returns the status of the Signaling Link
+     *
+     * @param $resourceId
+     * @return string The status of the resource
+     */
+    public function status($resourceId)
+    {
+        return $this->getSNMP()->get( self::OID_STATUS . '.' . $resourceId);
+    }
+
+    /**
+     * Returns the description of the Signaling Link
+     *
+     * @param $resourceId
+     * @return string The label of the resource
+     */
+    public function description($resourceId)
+    {
+        return $this->getSNMP()->get( self::OID_DESCRIPTION . '.' . $resourceId);
+    }
+
+    /**
+     * Returns the value of a counter of number of outbound bits on the Signaling Link
+     *
+     * @param $resourceId
+     * @return string The number of outbound bits that transited by the logical interface
+     */
+    public function txBits($resourceId)
+    {
+        return $this->getSNMP()->get( self::OID_TX_BITS_TOTAL . '.' . $resourceId);
+    }
+
+    /**
+     * Returns the value of a counter of number of inbound bits on the Signaling Link
+     *
+     * @param $resourceId
+     * @return string The number of inbound bits that transited by the logical interface
+     */
+    public function rxBits($resourceId)
+    {
+        return $this->getSNMP()->get( self::OID_RX_BITS_TOTAL . '.' . $resourceId);
+    }
+
+    /**
+     * Returns the value of a counter of number of outbound Message Service Units (MSU) on the Signaling Link
+     *
+     * @param $resourceId
+     * @return string The number of outbound Message Service Units (MSU) that transited by the logical interface
+     */
+    public function txMsu($resourceId)
+    {
+        return $this->getSNMP()->get( self::OID_TX_MSU_TOTAL . '.' . $resourceId);
+    }
+
+    /**
+     * Returns the value of a counter of number of inbound Message Service Units (MSU) on the Signaling Link
+     *
+     * @param $resourceId
+     * @return string The number of inbound Message Service Units (MSU) that transited by the logical interface
+     */
+    public function rxMsu($resourceId)
+    {
+        return $this->getSNMP()->get( self::OID_RX_MSU_TOTAL . '.' . $resourceId);
+    }
+}
--- src/OSS_SNMP/MIBS/Squire/STP/System.php
+++ src/OSS_SNMP/MIBS/Squire/STP/System.php
@@ -12,24 +12,10 @@ class System extends \OSS_SNMP\MIB
     const OID_ASP_STATUS                        = '.1.3.6.1.2.1.2.2.34.0';
     const OID_ASP_DESC                          = '.1.3.6.1.2.1.2.2.34.26';
 
-    const OID_SIGLINK_STATUS                    = '.1.3.6.1.2.1.2.2.3.0';
-    const OID_SIGLINK_DESC                      = '.1.3.6.1.2.1.2.2.3.44';
-    const OID_SIGLINK_IN_SERVICE_TIMESTAMP      = '.1.3.6.1.2.1.2.2.3.15';
-    const OID_SIGLINK_OUT_SERVICE_TIMESTAMP     = '.1.3.6.1.2.1.2.2.3.16';
-    const OID_SIGLINK_TX_BITS                   = '.1.3.6.1.2.1.2.2.3.38';
-    const OID_SIGLINK_RX_BITS                   = '.1.3.6.1.2.1.2.2.3.41';
-    const OID_SIGLINK_TX_MSU                    = '.1.3.6.1.2.1.2.2.3.32';
-    const OID_SIGLINK_RX_MSU                    = '.1.3.6.1.2.1.2.2.3.35';
-
     const OID_CIRCUIT_STATUS                    = '.1.3.6.1.2.1.2.2.4.0';
     const OID_CIRCUIT_DESC                      = '.1.3.6.1.2.1.2.2.4.23';
     const OID_CIRCUIT_DESC_svi_v8_8_56          = '.1.3.6.1.2.1.2.2.4.21'; // older versions of Squire SVI, up to v8.8.56
     const OID_CIRCUIT_LOAD                      = '.1.3.6.1.2.1.2.2.4.16'; // CIC ???
-
-    const OID_LINKSET_TX_BITS                   = '.1.3.6.1.2.1.2.2.8.22';
-    const OID_LINKSET_RX_BITS                   = '.1.3.6.1.2.1.2.2.8.25';
-    const OID_LINKSET_TX_MSU                    = '.1.3.6.1.2.1.2.2.8.16';
-    const OID_LINKSET_RX_MSU                    = '.1.3.6.1.2.1.2.2.8.19';
 
     const OID_DISTANT_PC_STATUS                 = '.1.3.6.1.2.1.2.2.7.0';
     const OID_DISTANT_PC_DESC                   = '.1.3.6.1.2.1.2.2.7.15';
@@ -58,28 +44,6 @@ class System extends \OSS_SNMP\MIB
     public function aspDesc($resourceId)
     {
         return $this->getSNMP()->get( self::OID_ASP_DESC . '.' . $resourceId);
-    }
-
-    /**
-     * Returns the status of the Signaling Link
-     *
-     * @param $resourceId
-     * @return string The status of the resource
-     */
-    public function sigLinkStatus($resourceId)
-    {
-        return $this->getSNMP()->get( self::OID_ASP_STATUS . '.' . $resourceId);
-    }
-
-    /**
-     * Returns the description of the Signaling Link
-     *
-     * @param $resourceId
-     * @return string The label of the resource
-     */
-    public function sigLinkDesc($resourceId)
-    {
-        return $this->getSNMP()->get( self::OID_SIGLINK_DESC . '.' . $resourceId);
     }
 
 
